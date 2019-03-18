dbac75c21efed085b204171bff59fa4d920d8587
ClauFer <claudia.villarroel@fundacion-jala.org>
2019-03-18 18:55:49 -0400

    Archivos Python

---
+++ AritmeticOperatorClau.py
@@ -0,0 +1,39 @@
+
+#Practice Arithmetic's operators
+print "Practice Arithmetic's operators"
+num1=5
+numero2=2.1
+resultado1=num1+ numero2
+print resultado1
+resultado1= num1- numero2
+print resultado1
+resultado1= num1* numero2
+print resultado1
+resultado1 =num1/ numero2
+print resultado1
+resultado1 = num1 % numero2
+print resultado1
+
+#FUNCIONES
+print "FUNCTIONS"
+
+def operaciones(num1,num2):
+ suma= num1+num2
+ return suma
+
+operaciones (2,8)
+print operaciones(2,8)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
