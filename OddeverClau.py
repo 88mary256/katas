88692668ba174ee80857b8cce52c1c074b747c46
ClauFer <claudia.villarroel@fundacion-jala.org>
2019-03-19 14:54:36 -0400

    fix code review

--- OddeverClau.py
+++ OddeverClau.py
@@ -1,16 +1,9 @@
 # Create 1 script to determine is a number is odd or even (use single line statement if applies)
-def numero(num1):
-    if num1 % 2 == 0:
-        print "El numero es par"
-    if num1 % 2 == 1:
-        print "El numero es impar"
-    return numero(num1)
 
-
-numero(5)
-
-
-
-
-
-
+num = input("Introduce un numero: ")
+if num == 0:
+    print ("Este numero es par.")
+elif num % 2 == 0:
+    print ("Este numero es par")
+else:
+    print ("Este numero es impar")
