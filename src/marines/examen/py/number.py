9556311febaf4e3f99a3d437087658614281cdb9
88mary256 <mary.jlm.2006@gmail.com>
2019-03-20 16:34:31 -0400

    2: exercise capicua added

--- src/marines/examen/py/number.py
+++ src/marines/examen/py/number.py
@@ -2,6 +2,10 @@ def factorial(num):
     if (num == 0 or num == 1):
         return 1
     fact = 1
-    for index in range(2,num+1):
+    for index in range(2, num + 1):
         fact *= index
     return fact
+
+
+def capicua(num):
+    return str(num)[::-1] == str(num);
--- src/marines/examen/tests/number_tests.py
+++ src/marines/examen/tests/number_tests.py
@@ -4,8 +4,17 @@ from examen.py.number import *
 
 
 class TestTime(unittest.TestCase):
-    def test_days_in_month(self):
+    def test_factorial(self):
         self.assertEqual(factorial(0), 1)
         self.assertEqual(factorial(1), 1)
         self.assertEqual(factorial(5), 120)
         self.assertEqual(factorial(10), 3628800)
+
+    def test_capicua(self):
+        self.assertEqual(capicua(0), True)
+        self.assertEqual(capicua(1), True)
+        self.assertEqual(capicua(9), True)
+        self.assertEqual(capicua(10), False)
+        self.assertEqual(capicua(11), True)
+        self.assertEqual(capicua(1551), True)
+        self.assertEqual(capicua(1552), False)
