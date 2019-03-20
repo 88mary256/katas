909b063655fd72f5e97df8053a7dd50bfb09aab4
ClauFer <claudia.villarroel@fundacion-jala.org>
2019-03-20 17:24:17 -0400

    Practica Claudia Villarroel

---
+++ Claudia/Practica/Herencia.py
@@ -0,0 +1,16 @@
+class auto:
+    def acelerar(self):
+        pass
+
+    def frenar(self):
+        pass
+
+
+class toyota(auto):
+
+    def acelerar(self):
+        print("El auto acelera")
+
+    def frenar(self):
+        print("El auto frenar")
+
