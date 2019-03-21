4134eba56f804e5f9667a94e0d373eb68cf07fe9
rrchcr8 <CarlosRichter@fundacion-jala.org>
2019-03-21 11:02:45 -0400

    refactor some unit tests

--- practicaenclases/test_carlosbubbleSort.py
+++ practicaenclases/test_carlosbubbleSort.py
@@ -3,4 +3,8 @@ from unittest import TestCase
 
 class TestCarlosbubbleSort(TestCase):
     def test_carlosbubbleSort(self):
-        self.fail()
+        def test_carlosbubbleSort(self):
+            from practicaenclases.Practica import carlosbubbleSort
+            arr = [(3, "Richter"), (2, "Hernando"), (1, "Carlos"), (4, "Rodriguez")]
+            sortedarr = [(1, "Carlos"), (2, "Hernando"), (3, "Richter"), (4, "Rodriguez")]
+            self.assertLessEqual(carlosbubbleSort(arr), sortedarr, "ok")
--- practicaenclases/test_is_capicua.py
+++ practicaenclases/test_is_capicua.py
@@ -3,7 +3,9 @@ from unittest import TestCase
 
 class TestIs_capicua(TestCase):
     def test_is_capicua(self):
-        from practicaenclases.Practica import carlosbubbleSort
-        arr = [(3, "Richter"), (2, "Hernando"), (1, "Carlos"), (4, "Rodriguez")]
-        sortedarr= [(1, "Carlos"), (2, "Hernando"), (3, "Richter"), (4, "Rodriguez")]
-        self.assertLessEqual(carlosbubbleSort(arr),sortedarr,"ok")
+        from practicaenclases.Practica import is_capicua
+        self.assertEqual(is_capicua(121), "el numero es capicua", "ok")
+        self.assertEqual(is_capicua(1231), "el numero no es capicua", "ok")
+        self.assertEqual(is_capicua(1221), "el numero es capicua", "ok")
+        self.assertEqual(is_capicua(1), "el numero es capicua", "ok")
+        self.assertEqual(is_capicua(55), "el numero es capicua", "ok")
