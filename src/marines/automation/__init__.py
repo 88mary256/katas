e89652d447289e47f0c32df1ecae1244cdaede33
88mary256 <mary.jlm.2006@gmail.com>
2019-03-22 17:48:07 -0400

    D5: Added first automation test

---
+++ src/marines/automation/__init__.py
@@ -0,0 +1,5 @@
+# Instalar requests con pip
+# pip install requests
+# if pip no es reconocido en cmd añadir en variables de entorno:
+# %PYTHON_HOME%/Scripts
+# ejemplo: C:\Python27\Scripts
---
+++ src/marines/automation/create_project.py
@@ -0,0 +1,54 @@
+import json
+import requests
+import pprint
+
+#class AutomationForTodoLy():
+
+host = "https://todo.ly/api/"
+#def create_project():
+endpoint = host + "projects.json"
+header = {"Authorization": "Basic Z2F0aXRhMDEwMS5teG1AZ21haWwuY29tOjEyMzQ1Njc4",
+          "Content-Type": "application/json"}
+name = "My 2nd project"
+body = {"Content": name}
+response = requests.post(url=endpoint, data=json.dumps(body),headers=header)
+response_json = response.json()
+print("Create Project status code: %s" % response.status_code)
+print response
+pprint.pprint(response.json())
+print response.json()["Id"]
+
+assert name == response_json["Content"], "Failed"
+'''<Response [200]>
+{u'Children': [],
+ u'Collapsed': False,
+ u'Content': u'My 2nd project',
+ u'Deleted': False,
+ u'Icon': 0,
+ u'Id': 3782188,
+ u'IsOwnProject': True,
+ u'IsProjectShared': False,
+ u'IsShareApproved': False,
+ u'ItemOrder': 13,
+ u'ItemType': 2,
+ u'ItemsCount': 0,
+ u'LastSyncedDateTime': u'/Date(1553288790082)/',
+ u'LastUpdatedDate': u'/Date(1553288790083)/',
+ u'ParentId': None,
+ u'ProjectShareOwnerEmail': None,
+ u'ProjectShareOwnerName': None,
+ u'SyncClientCreationId': None}
+3782188
+'''
+
+'''
+tarea crear read update delete
+
+hacer 1 proyecto con su subproyecto y una tarea en el subprojecto
+Page Object Model
+Json schema validator
+Gherkin
+Behave tool
+
+Pivotaltracker.com 
+'''
