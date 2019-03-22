d95109b378f305f735c00771b7375e88b1e8c8ac
ClauFer <claudia.villarroel@fundacion-jala.org>
2019-03-22 18:24:45 -0400

    Ejercicios Katas

--- OddeverClau.py
+++
@@ -1,9 +0,0 @@
-# Create 1 script to determine is a number is odd or even (use single line statement if applies)
-
-num = input("Introduce un numero: ")
-if num == 0:
-    print ("Este numero es par.")
-elif num % 2 == 0:
-    print ("Este numero es par")
-else:
-    print ("Este numero es impar")
