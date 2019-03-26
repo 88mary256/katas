84192916af3d8c00653f635dc0529c7dbf7b5859
ClauFer <claudia.villarroel@fundacion-jala.org>
2019-03-26 11:55:49 -0400

    Slice3 Practice

---
+++ Slice3/Function1y2.py
@@ -0,0 +1,21 @@
+# Function 1, Slice 3:
+def mi_lista(number):
+nro= input("Inserte el nro de elementos en la lista: ")
+for nro in range (len(List)):
+        id = int(List[i])
+        if (1 <= id <= 100):
+            add_to_dic(dic["userID"], id)
+            print_user_group(id)
+            # dict["userID"].append(id)
+    print dic
+
+# Function 2, Slice 3:
+
+for i in range(len(nameList)):
+    name = nameList[i]
+    if (len(name) <= 8 and name == name.lower()):
+        # dict["userName"].append(name)
+        add_to_dic(dic["userName"], name)
+
+def printArray(array):
+    print array
